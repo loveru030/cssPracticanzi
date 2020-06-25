07692eb33f82606b1cf0ceeeebc32365f6908735
loveru030 <62735773+loveru030@users.noreply.github.com>
2020-06-25 14:56:19 +0900

    준렬씨가 수정해줌

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/cssPracticanzi/',
+       // '/cssPracticanzi/manifest.webmanifest',
+       '/cssPracticanzi/index.js',
+       '/cssPracticanzi/index.html',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
